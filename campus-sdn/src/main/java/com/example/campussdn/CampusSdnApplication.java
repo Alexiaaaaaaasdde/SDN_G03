526bd2d2cdbc8916b74834aca4939596dfbc128b
AlexiaPT <apalaciost@pucp.edu.pe>
2025-06-28 14:15:50 -0500

    v1 sdn campus

---
+++ campus-sdn/src/main/java/com/example/campussdn/CampusSdnApplication.java
@@ -0,0 +1,13 @@
+package com.example.campussdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSdnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusSdnApplication.class, args);
+    }
+
+}
---
+++ campus-sdn/src/test/java/com/example/campussdn/CampusSdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campussdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSdnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
